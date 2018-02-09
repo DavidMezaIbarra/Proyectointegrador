14402c0aea688312d8086cdb439151d7b1185232
David <davidmezaibarra1995@gmail.com>
2018-02-08 19:16:09 -0700

    agregue panel admon pagina info

---
+++ panelAdmin1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<div class="container" style="background-color: red; height: 500px;">
+		
+	</div>
+</body>
+</html>
